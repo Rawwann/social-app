4b9e4a93292b78709dd5149acff8636501cab794
Rawan Hany <rawanhany2026@gmail.com>
2026-02-26 22:58:00 +0200

    delete helmet-async

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import { ToastContainer } from 'react-toastify';
 import { Offline } from 'react-detect-offline';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Alert } from '@heroui/react';
-import { HelmetProvider } from 'react-helmet-async';
 
 const queryClient = new QueryClient()
 
@@ -14,21 +13,19 @@ const queryClient = new QueryClient()
 function App() {
   return (
     <>
-      <HelmetProvider>
-        <QueryClientProvider client={queryClient}>
-          <AuthProvider>
-            <RouterProvider router={router} />
-          </AuthProvider>
-        </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
+      </QueryClientProvider>
 
-        <ToastContainer theme='colored' position="top-center" />
+      <ToastContainer theme='colored' position="top-center" />
 
-        <Offline>
-          <div className="fixed top-4 left-1/2 -translate-x-1/2 z-50 w-full max-w-md px-4">
-            <Alert color="danger" title="You are currently offline. Please check your internet connection." />
-          </div>
-        </Offline>
-      </HelmetProvider>
+      <Offline>
+        <div className="fixed top-4 left-1/2 -translate-x-1/2 z-50 w-full max-w-md px-4">
+          <Alert color="danger" title="You are currently offline. Please check your internet connection." />
+        </div>
+      </Offline>
     </>
 
   );
